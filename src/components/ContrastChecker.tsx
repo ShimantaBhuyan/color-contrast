7887d523f06da4ca5a1c8cbb61b62f4ec20cb058
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2022-10-09 18:23:45 +0530

    feat: make the ui responsive

--- src/components/ContrastChecker.tsx
+++ src/components/ContrastChecker.tsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 // import { APCAcontrast, sRGBtoY, displayP3toY, colorParsley } from "bridge-pca";
 
 import styled from "styled-components";
-import { BRAND_TEXT_COLORS, StyledColSection, StyledText } from "../styles/global";
+import { StyledColSection, StyledText } from "../styles/global";
+
 import SwapIcon from "url:../assets/swap.svg";
+import { BRAND_TEXT_COLORS, devices } from "../constants";
 
 const ContrastChecker = () => {
   const [textColor, setTextColor] = useState("#FEF3C7");
@@ -36,7 +38,13 @@ const ContrastChecker = () => {
             Text Color
           </StyledText>
           <StyledColorPickerIcon color={textColor} />
-          <StyledInputs type="text" name="textColor" value={textColor} onChange={handleTextColorChange} />
+          <StyledInputs
+            type="text"
+            name="textColor"
+            value={textColor}
+            onChange={handleTextColorChange}
+            placeholder="Enter text color"
+          />
         </StyledColSection>
 
         <SwapButton onClick={swapColors} type="button">
@@ -48,7 +56,13 @@ const ContrastChecker = () => {
             Background Color
           </StyledText>
           <StyledColorPickerIcon color={bgColor} />
-          <StyledInputs type="text" name="bgColor" value={bgColor} onChange={handleBgColorChange} />
+          <StyledInputs
+            type="text"
+            name="bgColor"
+            value={bgColor}
+            onChange={handleBgColorChange}
+            placeholder="Enter background color"
+          />
         </StyledColSection>
       </StyledForm>
 
@@ -59,19 +73,40 @@ const ContrastChecker = () => {
   );
 };
 
-const Wrapper = styled(StyledColSection)``;
+const Wrapper = styled(StyledColSection)`
+  z-index: 1;
+  margin-top: -10%;
+
+  @media ${devices.mobileL} {
+    margin-top: 10%;
+  }
+`;
 
 const StyledForm = styled.form`
   display: flex;
   justify-content: center;
   grid-gap: 20px;
   gap: 20px;
+
+  @media ${devices.mobileL} {
+    flex-direction: column;
+    align-items: center;
+    grid-gap: 10px;
+    gap: 10px;
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 `;
 
 const StyledInputs = styled.input`
   padding: 5px;
   border: 3px solid #cbf3f0;
   border-radius: 5px;
+
+  @media ${devices.mobileL} {
+    width: 100%;
+  }
 `;
 
 const StyledColorPickerIcon = styled.div<{ color: string }>`
@@ -93,6 +128,11 @@ const StyledTextDisplay = styled.div<{ textColor: string; bgColor: string }>`
   text-align: center;
   font-size: 24px;
   margin-top: 20px;
+
+  @media ${devices.mobileL} {
+    width: 90%;
+    height: 200px;
+  }
 `;
 
 const SwapButton = styled.button`
@@ -101,6 +141,10 @@ const SwapButton = styled.button`
   height: 30px;
   border-radius: 15px;
   cursor: pointer;
+
+  @media ${devices.mobileL} {
+    transform: rotateZ(90deg);
+  }
 `;
 
 export default ContrastChecker;
